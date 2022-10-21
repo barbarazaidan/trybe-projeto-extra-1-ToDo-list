349f3bf041cfc96733b18db715ae0d054ae55686
bárbara-valdez-zaidan <babivaldez@gmail.com>
2022-10-21 19:19:14 -0300

    finalizei o requisito 8

--- script.js
+++ script.js
@@ -9,31 +9,34 @@ const inserindoTarefas = () => {
   novaTarefa.value = '';
 };
 
-const criandoClasse = () => {
+// utilizei o recurso event para conseguir isolar o item selecionado no momento e, assim, analisar os demais itens da lista, manipulando o background para estar em apenas um item a cada click
+function selecaoUnica(evento) {
   const itemDaLista = document.getElementsByTagName('li');
   for (let index = 0; index < itemDaLista.length; index += 1) {
-    if (itemDaLista[index].style.backgroundColor = 'gray') {
-      itemDaLista[index].classList.add('marcado');
+    if (itemDaLista[index] !== evento.target) {
+      itemDaLista[index].style.backgroundColor = 'white'
     }
   }
 };
 
-const verificandoClasse = () => {
-  const itemDaLista = document.getElementsByTagName('li');
-  for (let index = 0; index < itemDaLista.length; index += 1) {
-    
-  }
-};
+// function mudandoBackground() {
+//   const itemDaLista = document.getElementsByTagName('li');
+//   for (let index = 0; index < itemDaLista.length; index += 1) {
+//     if (itemDaLista[index].className === 'marcado') {
+//       itemDaLista[index].style.backgroundColor = 'gray';
+//     }
+//   }
+// };
 
 // para verificar se houve algum clique nos itens da lista, precisei colocar um escutador em cada item. Antes disso, criei um loop que vai percorrer os todos os itens
 const selecionandoTarefas = () => {
   const itemDaLista = document.getElementsByTagName('li');
   for (let index = 0; index < itemDaLista.length; index += 1) {
-    itemDaLista[index].addEventListener('click', () => {
-      itemDaLista[index].style.backgroundColor = 'gray';
-      criandoClasse();
-      verificandoClasse();
-    });
+    itemDaLista[index].addEventListener('click', (event) => {
+      // event.target.classList.add('marcado')
+      event.target.style.backgroundColor = 'gray'
+      selecaoUnica(event)
+    }); 
   }
 };
 
