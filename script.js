f5a1a36bd1f67ffac19bef34f2a5c72070e70197
bárbara-valdez-zaidan <babivaldez@gmail.com>
2022-10-28 12:06:51 -0300

    finalizei o requisito 11

--- script.js
+++ script.js
@@ -2,6 +2,7 @@ const criarTarefa = document.getElementById('criar-tarefa');
 const novaTarefa = document.getElementById('texto-tarefa');
 const listaTarefas = document.getElementById('lista-tarefas');
 const botaoApaga = document.getElementById('apaga-tudo');
+const botaoRemoverFinalizados = document.getElementById('remover-finalizados');
 
 const inserindoTarefas = () => {
   const itemDaLista = document.createElement('li');
@@ -69,7 +70,42 @@ const ajustandoLista = () => {
 const apagandoTarefas = () => {
   listaTarefas.innerHTML = '';
 };
+// ---------------------------------------------------------------------------
+// O código abaixo resulta em um HTML collection, assim quando ele apaga o elemento no index 0, a lista é reordenada, ou seja, o elemento que estava no index 1, vai para o 0. Isso faz com que o loop não se desenvolva corretamente, afinal o index pelo qual ele já passou acaba de ter um novo elemento com a reordenação (é a questão de pilha e lista que preciso estudar melhor).
+
+// const apagandoFinalizados = () => {
+//   const itemCompleto = document.getElementsByClassName('completed');
+//   console.log(itemCompleto)
+//   for (let index = 0; index < itemCompleto.length; index += 1) {
+//     itemCompleto[index].remove();
+//   }
+// };
+
+// ----------------------------------------------------------------------------
+
+// Caso eu queira que o código funciona usando HTML collection, preciso fazer o for de trás para frente, pois isso faz com que a reordenação não atrapalhe o processo do loop, afinal não entrará mais nenhum elemento nos últimos índices, pois a pilha vai descendo e não subindo.
+
+// const apagandoFinalizados = () => {
+//   const itemCompleto = document.getElementsByClassName('completed');
+//   console.log(itemCompleto)
+//   for (let index = itemCompleto.length -1 ; index >= 0; index -= 1) {
+//     itemCompleto[index].remove();
+//   }
+// };
+
+// ------------------------------------------------------------------------
+
+// Usando o querySelectorAll, eu trabalho com uma nodeList, isso faz com que o loop do for funcione normalmente seja com contador crescente ou decrescente.
+
+const apagandoFinalizados = () => {
+  const itemCompleto = document.querySelectorAll('.completed');
+  // console.log(itemCompleto)
+  for (let index = 0; index < itemCompleto.length; index += 1) {
+    itemCompleto[index].remove();
+  }
+};
 
 criarTarefa.addEventListener('click', ajustandoLista);
 listaTarefas.addEventListener('dblclick', riscandoTarefas);
 botaoApaga.addEventListener('click', apagandoTarefas);
+botaoRemoverFinalizados.addEventListener('click', apagandoFinalizados);
